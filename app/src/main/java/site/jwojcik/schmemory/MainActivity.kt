cf931d5ad5fee782b0bcc288376c636f1c60e039
SunnieTea <156830537+SunnieTea@users.noreply.github.com>
2026-04-28 12:00:48 -0400

    dark mode starting to work

--- app/src/main/java/site/jwojcik/schmemory/MainActivity.kt
+++ app/src/main/java/site/jwojcik/schmemory/MainActivity.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import site.jwojcik.schmemory.data.AppPreferences
+import site.jwojcik.schmemory.data.PreferenceStorage
 import site.jwojcik.schmemory.ui.theme.SchmemoryTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +22,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SchmemoryTheme {
+            val store = PreferenceStorage(this)
+            val prefs by store.appPreferencesFlow.collectAsStateWithLifecycle(
+                initialValue = AppPreferences(
+                    darkMode = false,
+                    fontScale = 1f,
+                    dyslexicFont = false
+                )
+            )
+
+            SchmemoryTheme (
+                darkTheme = prefs.darkMode,
+                fontScale = prefs.fontScale,
+                dyslexicFont = prefs.dyslexicFont
+            ) {
                 SchmemoryApp()
             }
         }
--- app/src/main/java/site/jwojcik/schmemory/ui/theme/Theme.kt
+++ app/src/main/java/site/jwojcik/schmemory/ui/theme/Theme.kt
@@ -1,8 +1,6 @@
 package site.jwojcik.schmemory.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -10,6 +8,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -21,21 +20,13 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun SchmemoryTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean,
+    fontScale: Float,
+    dyslexicFont: Boolean,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -48,6 +39,12 @@ fun SchmemoryTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val dyslexicFamily = if (dyslexicFont) {
+        FontFamily.Default
+    } else {
+         Typography.bodyLarge.fontFamily
     }
 
     MaterialTheme(
