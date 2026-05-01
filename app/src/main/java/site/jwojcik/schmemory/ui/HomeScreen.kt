a721c53602a715e831140f38f28906315319eb81
PatrickLiszewski <pj03lisz@siena.edu>
2026-04-30 21:15:45 -0400

    Work in progress button animations, waiting for more white space on icons.

--- app/src/main/java/site/jwojcik/schmemory/ui/HomeScreen.kt
+++ app/src/main/java/site/jwojcik/schmemory/ui/HomeScreen.kt
@@ -1,9 +1,16 @@
 package site.jwojcik.schmemory.ui
 
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -33,6 +40,7 @@ import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
@@ -103,7 +111,7 @@ fun HomePageScreen(
                 onClick = onSpeechesClick,
                 modifier = Modifier
                     .align(Alignment.TopStart)
-                    .padding(top = 230.dp, start = 32.dp)
+                    .padding(top = 220.dp, start = 32.dp)
             )
 
 
@@ -115,7 +123,7 @@ fun HomePageScreen(
                 onClick = onScenesClick,
                 modifier = Modifier
                     .align(Alignment.TopEnd)
-                    .padding(top = 230.dp, end = 32.dp)
+                    .padding(top = 220.dp, end = 32.dp)
             )
 
 
@@ -127,7 +135,7 @@ fun HomePageScreen(
                 onClick = onSettingsClick,
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
-                    .padding(bottom = 190.dp)
+                    .padding(bottom = 170.dp)
             )
         }
 
@@ -173,7 +181,7 @@ fun MorphingButton(
 
     Box(
         modifier = modifier
-            .size(80.dp)
+            .size(110.dp)
             .clip(MorphPolygonShape(morph, animatedProgress))
             .background(Green)
             .clickable(
@@ -190,8 +198,93 @@ fun MorphingButton(
         Icon(
             painter = painterResource(id = iconRes),
             contentDescription = contentDescription,
-            modifier = Modifier.size(48.dp), // Reduced size to center properly in morph shape
+            modifier = Modifier.size(64.dp),
             tint = Color.Unspecified
+        )
+        RotatingScallopedProfilePic(iconRes, contentDescription)
+    }
+}
+
+class CustomRotatingMorphShape(
+    private val morph: Morph,
+    private val percentage: Float,
+    private val rotation: Float
+) : Shape {
+
+    private val matrix = Matrix()
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        // Below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+        // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+        matrix.scale(size.width / 2f, size.height / 2f)
+        matrix.translate(1f, 1f)
+        matrix.rotateZ(rotation)
+
+        val path = morph.toPath(progress = percentage).asComposePath()
+        path.transform(matrix)
+
+        return Outline.Generic(path)
+    }
+}
+@Composable
+private fun RotatingScallopedProfilePic(
+    iconRes: Int,
+    contentDescription: String
+    ) {
+    val shapeA = remember {
+        RoundedPolygon(
+            12,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val shapeB = remember {
+        RoundedPolygon.star(
+            12,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val morph = remember {
+        Morph(shapeA, shapeB)
+    }
+    val infiniteTransition = rememberInfiniteTransition("infinite outline movement")
+    val animatedProgress = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "animatedMorphProgress"
+    )
+    val animatedRotation = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            tween(6000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "animatedMorphProgress"
+    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(iconRes),
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(
+                    CustomRotatingMorphShape(
+                        morph,
+                        animatedProgress.value,
+                        animatedRotation.value
+                    )
+                )
+                .size(75.dp)
         )
     }
 }
@@ -219,4 +312,3 @@ class MorphPolygonShape(
         return Outline.Generic(path)
     }
 }
-
